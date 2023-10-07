e010790e7f866b0e4f6481dbe4fca657c8834924
acciof3@gmail.com <accio-f3>
2023-10-07 22:21:48 +0530

    call method

--- Call.js
+++ Call.js
@@ -1,26 +1,39 @@
-let obj = {
-  a: 10,
+// let obj = {
+//   a: 10,
+// };
+
+// function abc() {
+//   console.log("inside window");
+//   console.log('inside fn->',this);
+// }
+
+// console.log(obj);
+
+// // dataStructure.method()
+// abc.call(obj);
+
+// console.log(obj);
+
+// abc();
+
+// // steps involved inside call method when you write funtionName.call(objName);
+
+// // 1. it attaches funtionName function with objName object.
+// // 2. it executes funtionName function with objName context -> how?????
+// //          2 A. -> objName.funtionName();
+// // 3. delete funtionName from objName
+
+
+
+
+let obj2 = {
+  obj3: {
+    ppoiui: 'this is obj3 string',
+  },
 };
 
-function abc() {
-  console.log("inside window");
-  console.log('inside fn->',this);
+function test(){
+    console.log(this);
 }
 
-console.log(obj);
-
-// dataStructure.method()
-abc.call(obj);
-
-console.log(obj);
-
-abc();
-
-// steps involved inside call method when you write funtionName.call(objName);
-
-// 1. it attaches funtionName function with objName object.
-// 2. it executes funtionName function with objName context -> how?????
-//          2 A. -> objName.funtionName();
-// 3. delete funtionName from objName object
-
-
+test.call(obj2.obj3);
