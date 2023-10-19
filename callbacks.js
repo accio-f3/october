dd65580817414597883a5959678d416e75b8a276
acciof3@gmail.com <accio-f3>
2023-10-19 22:27:41 +0530

    fetch

--- callbacks.js
+++ callbacks.js
@@ -1,18 +1,18 @@
-// // sync
+// // // sync
+// // console.log('start');
+// // console.log('mid');
+// // console.log('end');
+
+// // // async
+// // console.log("start");
+// // setTimeout(() => {
+// //   console.log("mid");
+// // }, 500);
+// // console.log("end");
+
 // console.log('start');
-// console.log('mid');
-// console.log('end');
 
-// // async
-// console.log("start");
-// setTimeout(() => {
-//   console.log("mid");
-// }, 500);
-// console.log("end");
-
-// console.log('start');
-
-// function intro(name) {
+// function intro(name) { 
 //     // async
 //   setTimeout(() => {
 //     return `Hi I am ${name}`;
@@ -21,46 +21,48 @@
 // //   return undefined;
 // }
 // const introMessage = intro('subham');
+// console.log(introMessage);
+
 
 // console.log('end');
 
-// callback hack
-// console.log("start");
+// // callback hack
+// // console.log("start");
 
-function intro(name, cb) {
-  setTimeout(() => {
-    // here we are calling
-    cb(`Hi I am ${name}`);
-  }, 1000);
-  //   return undefined;
-}
+// // function intro(name, cb) {
+// //   setTimeout(() => {
+// //     // here we are calling
+// //     cb(`Hi I am ${name}`);
+// //   }, 1000);
+// //   //   return undefined;
+// // }
 
-// this is just defi of callback
-intro("subham", (message) => {
-  console.log(message);
-  //   I will call my study info after name is printed
-  whereDoIStudy("accio", (message) => {
-    console.log(message);
-    birthPlace("firozabad", (message) => {
-      console.log(message);
-    });
-  });
-});
+// // // this is just defi of callback
+// // intro("subham", (message) => {
+// //   console.log(message);
+// //   //   I will call my study info after name is printed
+// //   whereDoIStudy("accio", (message) => {
+// //     console.log(message);
+// //     birthPlace("firozabad", (message) => {
+// //       console.log(message);
+// //     });
+// //   });
+// // });
 
-// cart
-// order successful
-// proceed to pay
+// // cart
+// // order successful
+// // proceed to pay
 
 
-function whereDoIStudy(edTechName, cb) {
-  setTimeout(() => {
-    // here we are calling
-    cb(`Hi I study at ${edTechName}`);
-  }, 500);
-  //   return undefined;
-}
+// // function whereDoIStudy(edTechName, cb) {
+// //   setTimeout(() => {
+// //     // here we are calling
+// //     cb(`Hi I study at ${edTechName}`);
+// //   }, 500);
+// //   //   return undefined;
+// // }
 
-function birthPlace(city, cb) {
+ function birthPlace(city, cb) {
   setTimeout(() => {
     // here we are calling
     cb(`My birth place ${city}`);
@@ -68,15 +70,15 @@ function birthPlace(city, cb) {
   //   return undefined;
 }
 
-// whereDoIStudy('accio',(message)=>{
-//     console.log(message);
-// })
+// // // whereDoIStudy('accio',(message)=>{
+// // //     console.log(message);
+// // // })
 
-console.log("end");
+// // console.log("end");
 
-// promises
-// async await
+// // promises
+// // async await
 
-// intro -> 1000 ms
-// study -> 500 ms
-// birthPlace -> 300 ms
+// // intro -> 1000 ms
+// // study -> 500 ms
+// // birthPlace -> 300 ms
