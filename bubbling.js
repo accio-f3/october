3077d013104841a46b237426d0b6ee482798be32
acciof3@gmail.com <accio-f3>
2023-11-01 22:14:30 +0530

    trickle/bubbling

---
+++ bubbling.js
@@ -0,0 +1,25 @@
+const grandParent = document.getElementById("grandParent");
+
+const parent = document.getElementById("parent");
+
+const child = document.getElementById("child");
+
+// there's one more argument of addEventListener
+// 1st is type of event
+// 2nd is function which has to be triggered
+// 3rd is true/ false
+// by default 3rd arg is false
+
+
+grandParent.addEventListener("click", (event) => {
+    event.stopPropagation();
+  console.log("grand parent clicked");
+},false);
+
+parent.addEventListener("click", () => {
+  console.log("parent clicked");
+},true);
+
+child.addEventListener("click", (event) => {
+  console.log("child clicked");
+},false);
