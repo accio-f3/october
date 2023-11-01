d423bc5362e657d633ec54a54d591463ccf93cfc
acciof3@gmail.com <accio-f3>
2023-11-01 21:07:54 +0530

    throttle

--- throttle.js
+++ throttle.js
@@ -1,6 +1,10 @@
 // shotgun
 // machine
 
+// throttleFn -> it's a technique to limit the execution
+// of an event when this event triggers continuesly 
+// due to user's actions
+
 let machineCount = 0;
 let shotgunCount = 0;
 
@@ -34,3 +38,19 @@ trigger.addEventListener("click", () => {
   normalCountDisplay.innerText = machineCount;
   actualShotGun();
 });
+
+
+
+    // console.log(new Date().getTime());
+    // for(let i=0;i<1000;i++){
+    //     console.log('g');
+    // }
+    // console.log(new Date().getTime());
+
+
+    function test(){
+        return;
+    }
+
+
+    console.log(test());
